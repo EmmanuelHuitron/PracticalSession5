e43a8e92e432ea6ca163a890e27c29eaeb6e8d13
EmmanuelHuitron <54409058+EmmanuelHuitron@users.noreply.github.com>
2020-11-10 20:24:54 -0600

    Create program.py

---
+++ program.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+A = int(input())
+B = int(input())
+msj = int(input())
+
+M = a*b-1
+e = A*M+a
+d = B*M+b
+n = (e*d-1)/M
+
+
+if input().strip().upper() == 'E':
+	print(int(msj*e%n), end='')
+else
+	print(int(msj*d%n), end='')
